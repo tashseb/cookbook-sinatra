e603b399fbce6ceb10380cff69f5908ef56dea31
Tash Cruz <sebastiantashika@gmail.com>
2022-10-17 16:23:33 +0900

    added several files

---
+++ app.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path(__dir__)
+end
+
+get "/" do
+  erb :index
+end
+
+get "/about" do
+  erb :about
+end
+
+get "/team/:username" do
+  puts params[:username]
+  "The username is #{params[:username]}"
+  erb :team
+end
